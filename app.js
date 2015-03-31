c6a844c592a5e643ca925f81fae7245c5a5a8294
Rabea Gleissner <rabeagleissner@gmail.com>
2015-03-31 10:48:30 +0100

    set up jade view template

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ var express = require('express');
 var app     = express();
 var port    = process.env.PORT || 3000;
 
+app.set('views', './views');
+app.set('view engine', 'jade');
+
 app.get('/', function(req, res) {
-  res.send('Hello World');
+  res.render('index');
 });
 
 
