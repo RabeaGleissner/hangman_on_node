1908a23b7c5988ac363fe37eeeb263be87026702
Rabea Gleissner <rabeagleissner@gmail.com>
2015-03-31 11:23:45 +0100

    installed morgan middleware

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var morgan = require('morgan');
 var app     = express();
 var port    = process.env.PORT || 3000;
 
 app.set('views', './views');
 app.set('view engine', 'jade');
+
+// Middelware
+app.use(morgan('dev'));
 
 app.get('/', function(req, res) {
   res.render('index');
