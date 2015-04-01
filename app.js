7bcc787acfccb466302ca0c1f8c9338f79b9a690
Rabea Gleissner <rabeagleissner@gmail.com>
2015-04-01 10:31:29 +0100

    added routing

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
+var morgan  = require('morgan');
 var express = require('express');
-var morgan = require('morgan');
 var app     = express();
 var port    = process.env.PORT || 3000;
+var router  = express.Router();
 
 app.set('views', './views');
 app.set('view engine', 'jade');
 
 // Middelware
 app.use(morgan('dev'));
+app.use('/', router);
 
-app.get('/', function(req, res) {
-  res.render('index');
+
+router.get('/', function(req, res) {
+  res.render('index', { header: 'index!'});
+});
+
+router.get('/words', function(req, res) {
+  res.render('words', { header: 'contact!'});
 });
 
 
